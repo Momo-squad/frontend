01095b390d4dd1509fa4d86f00a289edc35d6350
bibekshhh <bibekrock2019@gmail.com>
2023-01-09 19:56:56 +0545

    added insights reports

--- src/dashboard/user-dashboard/components/insightReports.jsx
+++ src/dashboard/user-dashboard/components/insightReports.jsx
@@ -1,6 +1,36 @@
+import "../styles/insightReports.css"
+
 const InsightReports = () => {
     return(
-        <>This is isnights report page</>
+        <>
+        <label htmlFor="#" className="content-header">Past Reports</label>
+        <div className="insight-reports-container">
+            <div className="options"></div>
+
+            <div className="reports-list">
+                <div className="report-header thead">
+                    <span className="th">Report Id</span>
+                    <span className="th">Farm Name</span>
+                    <span className="th">Report type</span>
+                    <span className="th">Generated on</span>
+                    <span className="th">Actions</span>
+                </div>
+                <div className="reports">
+                    <div className="report">
+                        <div className="item">102</div>
+                        <div className="item">Farm A</div>
+                        <div className="item">Soil Insight</div>
+                        <div className="item">01-05-22</div>
+                        <div className="item action-btns">
+                            <button className="view"><i className="bi bi-eye"></i></button>
+                            <button className="download"><i className="bi bi-printer"></i></button>
+                            <button className="remove"><i className="bi bi-trash3"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
 
